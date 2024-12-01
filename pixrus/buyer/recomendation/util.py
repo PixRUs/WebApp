b64cb282fe9cbe39114d3648da0488836a9a548c
Dothan <dbardich@mail.yu.edu>
2024-12-01 00:25:07 -0500

    small change

--- pixrus/buyer/recomendation/util.py
+++ pixrus/buyer/recomendation/util.py
@@ -1,6 +1,4 @@
-from buyer.models import Buyer
-from pixrus.tasks.update_pick_and_user_data import type_of_pick
-from seller.models import Seller
+
 from product.models import HistoricalPick, ActivePick
 import heapq
 import math
@@ -14,7 +12,7 @@ def get_recommended_picks(buyer,num_of_picks):
     Returns recommended sellers based on a specific buyer
     The recommendations are based of the risk taken by buyer, type of sport, league and
     """
-    all_historical_buyer_picks  = buyer.active_picks_buyer_access.all()
+    all_historical_buyer_picks  = buyer.historical_pick_buyer.all()
     ##Assign a value for the type of rist taken.
 
     total_risk_value = 0
@@ -23,35 +21,40 @@ def get_recommended_picks(buyer,num_of_picks):
     amount_of_units = 0
 
     for pick in all_historical_buyer_picks:
-        if pick.type_of_pick == 'h2h':
-            total_risk_value += get_probability(pick.pick_data['odds'])
-        else:
-            total_risk_value += 0.5
-        type_of_pick += get_type_of_pick(pick.game_event_result)
-        amount_of_units += pick.pick_data['bet_amount']
+        sport = pick.game_event_result['sport']
+        league = pick.game_event_result['league']
+        pick_prob,type,units = calculate_pick_values(category=[sport,league],pick_data=pick.pick_data,game_data=pick.game_event_result)
+        total_risk_value += pick_prob
+        amount_of_units += units
+        type_of_pick += type
 
     average_amount_of_units = amount_of_units / total_picks
     average_pick_risk = total_risk_value / total_picks
     average_type_of_pick = type_of_pick / amount_of_units
 
     class Node:
-        def __init__(self, dist, pick):
+        def __init__(self, dist, act_pick):
             self.dist = dist
-            self.pick = pick
+            self.pick = act_pick
 
         def __lt__(self, other):
             return self.dist < other.dist
     all_active_picks = ActivePick.objects.filter(is_free=True)
     pq = []
+
     for active_pick in all_active_picks:
-        risk = get_probability(active_pick.pick_data['odds'])
-        type = get_type_of_pick(active_pick.game_data)
-        units = get_probability(active_pick.pick_data['bet_amount'])
+        sport = active_pick.game_data['sport']
+        league = active_pick.game_data['league']
+        risk,type,units = calculate_pick_values(category=[sport,league],pick_data=active_pick.pick_data,game_data=active_pick.game_data)
         distance = calculate_distance(risk_1=average_pick_risk,units_1=average_amount_of_units,type_of_pick_1=average_type_of_pick,units_2=units,type_of_pick_2=type,risk_2=risk)
         heapq.heappush(pq,Node(distance,active_pick))
-    return heapq.nsmallest(num_of_picks, pq), average_amount_of_units, average_pick_risk, average_type_of_pick
+    res = []
+    k = heapq.nsmallest(min(num_of_picks,all_active_picks.count()), pq)
+    for m in k:
+        res.append(m.pick)
+    return res
 
-    
+
 
 def get_probability(odd:str):
     if odd.startswith("-"):
@@ -61,10 +64,10 @@ def get_probability(odd:str):
     else:
         odd = int(odd)
         return 100 / (odd + 100)
-def get_type_of_pick(game_event_result):
-    sport = game_event_result['sport']
-    league = game_event_result['league']
+    
+def get_pick_category(sport,league):
     return type_of_pick_map[league+"_"+sport]
+
 
 def calculate_distance(risk_1,units_1,type_of_pick_1,risk_2,units_2,type_of_pick_2):
     """Euclidean distance"""
@@ -82,4 +85,11 @@ def get_hottest_picks():
     pass
 
 
-
+def calculate_pick_values(category,pick_data,game_data):
+    if type== 'h2h':
+        pick_prob = get_probability(pick_data['odds'])
+    else:
+        pick_prob = 0.5
+    category_val = get_pick_category(sport = category[0],league = category[1])
+    amount_of_units = int(pick_data['bet_amount'])
+    return pick_prob,category_val,amount_of_units
