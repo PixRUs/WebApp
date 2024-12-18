0a49cbc745cf6438795f307ce2302d24f2890dae
Dothan <dbardich@mail.yu.edu>
2024-12-18 14:52:20 -0500

    almost done with PL

--- pixrus/accounts/urls.py
+++ pixrus/accounts/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path , include
 from accounts.views import home, landing, profile_completion
+from social_django.urls import urlpatterns as social_auth_urls
 
 urlpatterns = [
     path('', landing, name='landing'),  # Landing page (accessible without login)
